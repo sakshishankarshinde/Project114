629ec26c409b814a8527386b0a1cb0951145fd3e
sakshishankarshinde <154904843+sakshishankarshinde@users.noreply.github.com>
2024-01-09 20:05:33 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+a = 33
+b = 33
+
+if b > a :
+    print("b is grater than a")
+elif b == a:
+    print("b is equal to a")    
---
+++ test2.py
@@ -0,0 +1,7 @@
+def my_function(name):
+    print(name + "Refsnes")
+
+my_function("Emil")
+my_function("Tobas")
+my_function("Linus")
+    
